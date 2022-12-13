d6e1cbb59e81a9ef589c150b70d2833ba59b2408
Alex Vergara <55516909+SentinalMax@users.noreply.github.com>
2022-12-13 03:00:33 -0600

    Update github_save.sh

--- github_save.sh
+++ github_save.sh
@@ -13,8 +13,7 @@ if [ -f "$FILE" ]; then
         echo "$FILE exists -> copying to /home/HTML"
         cp $FILE /home/HTML
         cd HTML
-
-        # Here you'll enter the email tied to your GitHub account, as well as your Github Username
+        
         git config --global user.email $EMAIL
         git config --global user.name $USERNAME
 
@@ -29,7 +28,7 @@ if [ -f "$FILE" ]; then
         && sudo apt install gh -y
 
         # Init Repo
-        echo "# This is a copy of your 'index.html' file that exists in this DIR '/var/www/html' on your azure machine. You can run 'git add index.html && git commit -a && git push -u origin main' after executing the program on your azure machine to keep a backup stored on your Github Account." > README.md
+        echo "# This is a copy of your 'index.html' file which exists in this DIR '/var/www/html' on your azure machine. You can run 'git add index.html && git commit -a && git push -u origin main' after executing the program on your azure machine to keep a backup stored on your Github Account." > README.md
         git init
         git add README.md && git add index.html
         git commit -am "initial commit"
